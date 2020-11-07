ded6b8e856b6dfdb99a2e6b336c653699166f5b9
danny <dantes817@gmail.com>
2020-11-07 23:08:56 +0000

    was added gem devise/ created user auth/identification/registr.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   config.hosts << "0cef68b73d874bd4be011c7f7aa884c5.vfs.cloud9.eu-central-1.amazonaws.com"
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
